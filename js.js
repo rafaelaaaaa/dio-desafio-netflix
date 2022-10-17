5f77e8ad1ee2022eb31cec3f56f2e18a77cf338d
Rafaela Araújo <101407412+rafaelaaaaa@users.noreply.github.com>
2022-10-17 17:01:30 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,37 @@
+// botão informações
+
+function showModal() {
+	let element = document.getElementById("informacoes");
+	element.classList.add("show-informacoes");
+}
+
+function hideModal() {
+	let element = document.getElementById("informacoes");
+	element.classList.remove("show-informacoes");
+}
+
+
+// botão trailer
+
+function showTrailer() {
+	let element = document.getElementById("trailer");
+	element.classList.add("show-trailer");
+}
+
+function hideTrailer() {
+	let element = document.getElementById("trailer");
+	element.classList.remove("show-trailer");
+}
+
+
+// notificações
+
+function showNotificacao() {
+	let element = document.getElementById("notificacao");
+	element.classList.add("show-notificacao");
+}
+
+function hideNotificacao() {
+	let element = document.getElementById("notificacao");
+	element.classList.remove("show-notificacao");
+}
